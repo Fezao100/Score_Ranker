e600287d02cd9f8d2eb0216eec7d011eaf7f9396
Fezao100 <goldfz@hotmail.com>
2024-08-02 15:57:50 -0300

    projeto final

--- desafio1/index.js
+++ desafio1/index.js
@@ -1,1 +1,23 @@
-console.log("oa")
+let nome = "Fezao100"
+let xp = 10
+let classe
+
+if(xp < 1000){
+    classe = "Ferro"
+} else if(xp >= 1001 && xp <= 2000) {
+    classe = "Bronze"
+}else if(xp >= 2001 && xp <= 5000) {
+    classe = "Prata"
+}else if(xp >= 5001 && xp <= 7000) {
+    classe = "Ouro"
+}else if(xp >= 7001 && xp <= 8000) {
+    classe = "Platina"
+}else if(xp >= 8001 && xp <= 9000) {
+    classe = "Ascendente"
+}else if(xp >= 9001 && xp <= 10000) {
+    classe = "Imortal"
+}else if(xp >= 10001) {
+    classe = "Radiante"
+}
+
+console.log("O Herói de nome " + nome + " está no nível " + classe)
